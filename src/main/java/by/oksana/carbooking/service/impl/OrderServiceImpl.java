3756baec6b5bc1ce6e78993f668457c3545798cd
OksanaMinchuk <oks.m89@mail.ru>
2018-09-25 06:56:07 +0300

    update

--- src/main/java/by/oksana/carbooking/service/impl/OrderServiceImpl.java
+++ src/main/java/by/oksana/carbooking/service/impl/OrderServiceImpl.java
@@ -1,14 +1,20 @@
 package by.oksana.carbooking.service.impl;
 
+import by.oksana.carbooking.dao.OrderDAO;
 import by.oksana.carbooking.model.Order;
 import by.oksana.carbooking.service.OrderService;
 import org.springframework.stereotype.Service;
 
+import javax.annotation.Resource;
+
 @Service
 public class OrderServiceImpl implements OrderService {
 
+    @Resource
+    private OrderDAO orderDao;
+
     @Override
     public void registerOrder(Order order) {
-
+        orderDao.registerOrder(order);
     }
 }
