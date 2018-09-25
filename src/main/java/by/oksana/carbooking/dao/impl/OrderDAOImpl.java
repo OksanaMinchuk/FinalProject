2abd6d6562f4bc90cc8a1da1c226c359db10ed8c
OksanaMinchuk <oks.m89@mail.ru>
2018-09-25 10:44:36 +0300

    udate OrderDao

--- src/main/java/by/oksana/carbooking/dao/impl/OrderDAOImpl.java
+++ src/main/java/by/oksana/carbooking/dao/impl/OrderDAOImpl.java
@@ -4,12 +4,28 @@ import by.oksana.carbooking.dao.OrderDAO;
 import by.oksana.carbooking.model.Car;
 import by.oksana.carbooking.model.Order;
 import by.oksana.carbooking.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 
 public class OrderDAOImpl implements OrderDAO {
+
+    private static  final String SQL_INSERT_ORDER_IN_HISTORY="INSERT INTO history (client_id, cars_id, fromCity, toCity) VALUES(?,?,?,?)";
+
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
+
     @Override
     public void registerOrder(Order order) {
-        Car car = new Car();
-        User user = new User();
+        jdbcTemplate.update(SQL_INSERT_ORDER_IN_HISTORY, new Object[]{
+                order.getUser().getId(),
+                order.getCar().getId(),
+//                order.getModel(),
+//                order.getDateFrom(),
+//                order.getDateTo(),
+                order.getFromCity(),
+                order.getToCity()
+        });
 
     }
 }
